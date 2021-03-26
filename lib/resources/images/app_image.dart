721d2466d646efe791fd9b9b7bec1fb2fb60fb35
Satish Kumar <satish754ss@gmail.com>
2021-03-26 16:13:05 +0530

    Host

--- lib/resources/images/app_image.dart
+++
@@ -1,7 +0,0 @@
-class AppImage {
-  static const _imagePath = 'assets/';
-  static const logo = _imagePath + 'logo.png';
-
-  /// network image
-  static const flutterLogo='https://tech.pelmorex.com/wp-content/uploads/2020/10/flutter.png';
-}
--- lib/view/home/right_home_drawer.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class RightBody extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(child: Text(''));
-  }
-}
